feb541e1f6bf47cc84488f6f5a50aebf3546f599
wasek23 <wasekbellah2311@gmail.com>
2020-03-06 15:15:54 +0600

    Update negative condition

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // Login button event listener
 const btnLogin = document.getElementById("btnLogin");
-btnLogin.addEventListener("click", function(){
+btnLogin.addEventListener("click", function () {
     document.getElementById("loginArea").style.display = "none";
     document.getElementById("transactionArea").style.display = "block";
 });
 
 
 // Transaction
-function getInputNumber(id){
+function getInputNumber(id) {
     const input = document.getElementById(id).value;
     const inputNumber = parseFloat(input);
     return inputNumber;
 }
 
-function updateAmount(id, amount){
+function updateAmount(id, amount) {
     const current = document.getElementById(id).innerText;
     const currentAmount = parseFloat(current);
     const totalAmount = currentAmount + amount;
@@ -22,22 +22,32 @@ function updateAmount(id, amount){
 
 // Deposit button event listener
 const btnDeposit = document.getElementById("btnDeposit");
-btnDeposit.addEventListener("click", function(){
+btnDeposit.addEventListener("click", function () {
     const depositAmount = getInputNumber("depositInput");
-    
-    updateAmount("currentDeposit", depositAmount);
-    updateAmount("currentBalance", depositAmount);
-    
-    document.getElementById("depositInput").value = "";
+
+    if (depositAmount < 0) {
+        alert("Deposit amount cannot be negative");
+    }
+    else {
+        updateAmount("currentDeposit", depositAmount);
+        updateAmount("currentBalance", depositAmount);
+
+        document.getElementById("depositInput").value = "";
+    }
 });
 
 // Withdraw button event listener
 const btnWithdraw = document.getElementById("btnWithdraw");
-btnWithdraw.addEventListener("click", function(){
+btnWithdraw.addEventListener("click", function () {
     const withdrawAmount = getInputNumber("withdrawInput");
 
-    updateAmount("currentWithdraw", withdrawAmount);
-    updateAmount("currentBalance", -1 * withdrawAmount);
+    if (withdrawAmount < 0) {
+        alert("Withdraw amount cannot be negative");
+    }
+    else {
+        updateAmount("currentWithdraw", withdrawAmount);
+        updateAmount("currentBalance", -1 * withdrawAmount);
 
-    document.getElementById("withdrawInput").value = "";
+        document.getElementById("withdrawInput").value = "";
+    }
 });
